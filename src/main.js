85e1c09aeb691eac2f4da986e041e98b70a483f3
Jesse White <jesse.white6@gmail.com>
2021-04-23 08:33:36 -0700

    add currency compare function.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchangeService from './js/currency-class.js'
+
+function currencyCompare(response) {
+    const baseExchange = response.base_code;
+    let compareExchange = respsonse.conversion_rates;
+    if (baseExchange === "USD") {
+        console.log(compareExchange);
+    } else {
+        console.log(response);
+    }
+}
 
 $(document).ready(function () {
-
+    $('form').submit(function (event) {
+        event.preventDefault();
+    });
 });
