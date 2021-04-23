6f3d0305a526889e61b3f926206427cff0aa0dae
Jesse White <jesse.white6@gmail.com>
2021-04-23 11:18:33 -0700

    WIP: add more error handling for unknown code.

--- src/main.js
+++ src/main.js
@@ -5,14 +5,17 @@ import './css/styles.css';
 import CurrencyExchangeService from './js/currency-class.js';
 
 function currencyCompare(response) {
-    const baseRate2 = response.base_code;
-    const compareRate2 = response.target_code;
+    const baseRate = response.base_code;
+    const compareRate = response.target_code;
     if (!response.result === "success") {
         console.log("that did not work for currency");
+    } else if (response.result === "supported-code") {
+        return console.log("It doesn't look like we have that currencie code")
     } else {
-        console.log(baseRate2);
-        console.log(compareRate2);
+        console.log(baseRate);
+        console.log(compareRate);
         console.log(response.conversion_rate);
+        return response.conversion_rate;
     }
 }
 
@@ -22,10 +25,12 @@ $(document).ready(function () {
 
         const baseRate = $("#base-rate").val();
         const compareRate = $("#rate-to-compare").val();
+        const currencyAmmount = $("#ammount").val();
 
         CurrencyExchangeService.getCurrency(baseRate, compareRate)
             .then(function (response) {
-                currencyCompare(response);
+                $("#current-value").html(`The current exchange rate from ${baseRate} to ${compareRate} is: ${currencyCompare(response)}`);
+                $("#conversion").html(`Your total converted exchange value from ${baseRate} to ${compareRate} is: ${currencyCompare(response) * currencyAmmount}`);
             });
 
     });
